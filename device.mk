e89c2859a8f162ad0609e6bfa525b98a2afec504
Shawn Huang <shuang@mozilla.com>
2014-01-20 14:59:18 +0800

    Add bluedroid library bluetooth.default to hammerhead

--- device.mk
+++ device.mk
@@ -208,6 +208,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bdAddrLoader
 
+# for Gecko to support bluedroid stack
+PRODUCT_PACKAGES += \
+    bluetooth.default
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
 
